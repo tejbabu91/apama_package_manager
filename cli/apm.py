88317bac34d41da34e3c69278b4941c1caea3e42
Yadav <ry@softwareag.com>
2019-10-22 17:37:59 +0530

    list of  all available packages

--- cli/apm.py
+++ cli/apm.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python
-import sys, os, getopt, pathlib
+import sys, os, getopt, pathlib, urllib.request, json
 
 # add common python scripts to the path
 sys.path.append(os.fspath(pathlib.Path(__file__).parent.parent.joinpath('common')))
@@ -18,7 +18,18 @@ def list_packages():
 	"""
 	list of packages
 	"""
+	print ("")
 	print ("showing list of packages:")
+	print ("")
+	contents = urllib.request.urlopen("http://127.0.0.1:5000/packages")
+	encoding = contents.info().get_content_charset('utf-8')
+	manifests = json.loads(contents.read().decode(encoding))
+	manifest_list = manifests["packages"]
+	for elem in manifest_list:
+		print(elem["name"] + "==" + elem["version"] if "version" in elem  else "")
+		if "dependencies" in elem:
+			for deps in elem["dependencies"]:
+				print(deps["name"] + "==" + deps["version"] if "version" in deps  else "")
 
 def install(package):
 	"""
--- run_server.sh
+++ run_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-python3 -m venv ~/package_server_venv
+python -m venv ~/package_server_venv
 
 source ~/package_server_venv/bin/activate
 
-pip3 install -r requirements.txt
+pip install -r requirements.txt
 
 export PYTHONPATH="."
-python3 package_server/app.py
+python package_server/app.py
