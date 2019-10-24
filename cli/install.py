bbeadeb40c98a68b193ab51dc150022006a692c8
Yadav <ry@softwareag.com>
2019-10-24 15:51:40 +0530

    temp changes

--- cli/install.py
+++ cli/install.py
@@ -1,25 +1,38 @@
-import urllib.request
+import json
+from model import *
+from deps import find_dependencies
 
 def run(args=None):
 	"""
 	"""
-	pkgs_list = []
-	if args.install is not None:
-		for item in args.install:
-			name=item
-			version=None
-			if ":" in item:
-				t = item.split(":")
-				name, version = t[0], t[1]
+	pkgs_requested = dict()
+	for item in args.install:
+		name=item
+		version=None
+		if ":" in item:
+			name, version = item.split(":")
+		pkgs_requested[name] = version
 
-			d = {"name":name, "version": version}
-			pkgs_list.append(d)
+	existingPackages = dict()
+	packageData = None
+	with open('apama_packages.json', 'r') as json_file:
+		packageData = json.load(json_file)
+		for k in packageData['dependencies']:
+			existingPackages[k['name']] = k['version']
 
-	print(pkgs_list)
-	
+	for k in pkgs_requested.keys():
+		if k in existingPackages: print(f'Package {k} already installed')
+
+	existingPackages.update(pkgs_requested)
+
+	packages_to_install = find_dependencies(existingPackages.items())
+
+	packageData['dependencies'] = [{'name': k, 'version': v.to_str()} for (k,v) in packages_to_install.items()]
+
+	with open('apama_packages.json', 'w') as json_file:
+		json.dump(packageData, json_file, indent=2)
+
 def add_arguments(parser):
 	"""
 	"""
 	parser.add_argument(dest="install", nargs="*")
-
-
