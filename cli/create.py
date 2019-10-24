c8f78dc42a4b57adcf754047316b999ea146641b
Sandeep Komarneni <Sandeep.Komarneni@softwareag.com>
2019-10-24 15:07:23 +0530

    add support for creating a project

--- cli/create.py
+++ cli/create.py
@@ -1,21 +1,26 @@
+import os, shutil, json
+
 def run(args=None):
 	"""
 	"""
-	pkgs_list = []
-	pkgs = vars(args)
-	for item in pkgs["install"]:
-		name=None
-		version=None
-		if ":" in item:
-			name, version = item.split(":")
-		else:
-			name = item
-		d = {"name":name, "version": version}
-		pkgs_list.append(d)
-	print(pkgs_list)
-	
+	projectPath = os.path.abspath(args.projectName)
+	if os.path.exists(projectPath):
+		raise Exception(f'Can\'t create {projectPath}, directory with the same name already exists')
+
+	os.makedirs(projectPath, exist_ok=True)
+
+	projectTemplate = {
+		'name': os.path.basename(projectPath),
+		'description': '',
+		'version': '1.0.0',
+		'tags': [],
+		'dependencies': [],
+	}
+
+	with open(os.path.join(projectPath, 'apama_packages.json'), 'w') as fp:
+		json.dump(projectTemplate, fp, indent=2)
+
 def add_arguments(parser):
 	"""
 	"""
-	parser.add_argument(dest="create")
-	
+	parser.add_argument(dest="projectName")
